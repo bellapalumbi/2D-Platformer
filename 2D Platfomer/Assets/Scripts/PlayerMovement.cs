4b86834d670eaa5639bd7224b1328986cdebdcc8
bellapalumbi <bpalumbi@youth.dhftc.org>
2018-01-24 09:25:06 -0500

    Started Working on Movement
    Getting Key presses to register and adding forces accordingly to Player
    capsule

---
+++ 2D Platfomer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float thrust;
+	//private Vector2 = (10,10);
+
+	void Start () {
+		//rb.AddForce (transform.up * thrust);
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown ("space") || Input.GetKeyDown(KeyCode.W)) {
+			Debug.Log("jump pressed");
+		}
+	}
+
+	void MoveLeft() {
+		
+	}
+
+	void MoveRight() {
+		
+	}
+
+}
