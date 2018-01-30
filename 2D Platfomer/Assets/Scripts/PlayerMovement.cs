4694a4255bec4e0d28382faf361970f3b7cbd7bb
bellapalumbi <bpalumbi@youth.dhftc.org>
2018-01-30 09:12:44 -0500

    Jumping and Moving Okay
    movement is in, it’s just stuttery and only okay. Needs work.

--- 2D Platfomer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platfomer/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,11 @@ public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody2D player_rb;
 	public float thrust;
+
+	public Vector2 blinkVelocity;
+	public float speedMultiplier = 10;
+
+	public RaycastHit2D hit;
 	
 
 	void Start () {
@@ -14,13 +19,32 @@ public class PlayerMovement : MonoBehaviour {
 	
 
 	void Update () {
-		if (Input.GetKeyDown ("space") || Input.GetKeyDown( KeyCode.W) == true) {
+		if (Input.GetKeyDown ("space") || Input.GetKeyDown( KeyCode.W) == true) { //if space or w pressed
 			print("jump");
-			Debug.DrawRay(transform.position, -Vector2.up, Color.cyan);
-			if(Physics2D.Raycast(transform.position, -Vector2.up)) {
-				Jump();
+			//Debug.DrawRay(new Vector2(transform.position.x, transform.position.y-.2f), -Vector2.up, Color.cyan);
+			hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y-0.2f), -Vector2.up,0.1f);
+				//Send a raycast with a length of 0.1 from just below the player's position downward (opp of up)
+				//and record what it hits in 'hit', a bool. 
+			if (hit.transform != null) { //if hit is not null
+					//Debug.Log(hit.transform.gameObject);
+				if(hit.transform.gameObject.tag == "Jumpable") {
+					Jump();
+						//if the object hit has the tag "Jumpable, then run Jump()
+				}
 			}
 		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			//print("A pressed");
+			blinkVelocity = new Vector2(-1,0);
+			MoveLeft();
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			blinkVelocity = new Vector2(1,0);
+			MoveRight();
+		}
+
 	}
 
 	void Jump() {
@@ -28,11 +52,12 @@ public class PlayerMovement : MonoBehaviour {
 		player_rb.AddForce(transform.up * thrust);
 	}
 	void MoveLeft() {
-		
+		Debug.Log( "Move Left called");
+		player_rb.MovePosition(player_rb.position + blinkVelocity * Time.deltaTime);
 	}
 
 	void MoveRight() {
-		
+		player_rb.MovePosition(player_rb.position + blinkVelocity * Time.deltaTime);
 	}
 
 }
